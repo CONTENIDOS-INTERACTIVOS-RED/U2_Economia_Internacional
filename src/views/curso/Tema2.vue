bc6bbe685dd2533a50fccce3a1d48fdf61f523bf
Francisco Lizcano <flizcanor@icloud.com>
2025-06-15 19:30:51 -0500

    se ajusta color y genera material.zip

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -9,7 +9,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/B9yibkR5UBg?si=BdJm7BT_ogpbBVOr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     #t_2_1.titulo-segundo(data-aos="flip-up")
       h2 #[span 2.1] Tipos de acuerdos comerciales
